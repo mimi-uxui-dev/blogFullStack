a779b5f0f547ae9596536f794deba0e61c8d1886
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-10-30 16:15:35 +0100

    AUTH : User model

---
+++ backend/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+
+const userSchema = new mongoose.Schema({
+     username: {
+          type: String,
+          trim: true,
+          required: true,
+          maxLength: 32,
+          unique: true,
+          index: true,
+          lowerCase: true
+     },
+     name: {
+          type: String,
+          trim: true,
+          required: true,
+          maxLength: 32,
+     },
+     email: {
+          type: String,
+          trim: true,
+          required: true,
+          lowercase: true,
+          unique: true
+     },
+     profile: {
+          type: String,
+          required: true
+     },
+     hashed_password: {
+          type: String,
+          required: true
+     },
+     salt: Number,
+     about: {
+          type: String
+     },
+     role: {
+          type: Number,
+          trim: true
+     },
+     photo: {
+          data: Buffer,
+          contentType: String
+     },
+     resetPasswordLink: {
+          data: String,
+          default: ''
+     }
+}, { timeStamp: true })
+
+module.exports = mongoose.model('User', userSchema)
