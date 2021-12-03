bb7151ed7016ddb285864c5e63452fb0ac2f3a79
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-12-03 11:02:28 +0100

    singin token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,51 +5,53 @@ const expressJwt = require('express-jwt')
 
 exports.signup = (req, res) => {
      User.findOne({ email: req.body.email }).exec((err, user) => {
-          if(user){
+          if (user) {
                return res.status(400).json({
                     error: 'Email is taken'
                })
           }
 
-          const {name, email, password} = req.body
+          const { name, email, password } = req.body
           let username = shortId.generate()
-          let profile = `${process.env.CLIENT_URL}/profile/${username}` 
+          let profile = `${process.env.CLIENT_URL}/profile/${username}`
 
-          let newUser = new User({name, email, password, profile, username})
+          let newUser = new User({ name, email, password, profile, username })
 
           newUser.save((err, success) => {
-               if(err){
+               if (err) {
                     return res.status(400).json({
                          error: err
                     })
                }
                res.json({
-                    message: 'Signup success! Please Signin.'
-               }) 
-               /* res.json({
+                    message: 'Signup success! Please Signin.',
                     user: success
-               }) */
+               })
           })
-
      })
 }
 
 exports.signin = (req, res) => {
-     const  {email, password} = req.body
+     const { email, password } = req.body
      // check if user exist 
-     User.findOne({email}).exec((err, user) => {
-          if(err || !user){
+     User.findOne({ email }).exec((err, user) => {
+          if (err || !user) {
                return res.status(400).json({
                     error: 'User with that email does not exist. Please Signup.'
                })
           }
-     } )
-     // authenticate
-     if(!user.authenticate(password)){
-          return res.status(400).json({
-               error: 'Email & password do not match.'
+          // authenticate
+          if (!user.authenticate(password)) {
+               return res.status(400).json({
+                    error: 'Email & password do not match.'
+               })
+          }
+          // generate a token && send to client
+          const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+          res.cookie('token', token, { expiresIn: '1d' })
+          const { _id, username, name, email, role } = user
+          return res.json({
+               token, user
           })
-     }
-     // generate a token && send to client
-     
+     })
 }
